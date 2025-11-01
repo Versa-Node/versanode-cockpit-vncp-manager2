f0fc7e6b0060b221acb0dcd832fc18d4ecfff160
Pieter Cawood <pietercawood@gmail.com>
2025-11-01 10:52:26 +0100

    Convert from Podman to Docker: Add stub Pod components and update gitignore
    - Add PodActions.jsx stub - Docker doesn't support pods like Podman
    - Add PodCreateModal.jsx stub - Docker doesn't support pods like Podman
    - Update .gitignore to allow committing build infrastructure for Docker version
    - Enables 'sudo make install' workflow for other developers

---
+++ src/PodCreateModal.jsx
@@ -0,0 +1,5 @@
+// Docker doesn't support pods like Podman does, so this is a stub component
+export const PodCreateModal = ({ users, onAddNotification }) => {
+    // Return null since Docker doesn't have pod functionality
+    return null;
+};
